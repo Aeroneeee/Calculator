34b5523d558876f69ab505632a0627af014e0e5e
Aeroneeee <pamintuan.aerone@gmail.com>
2020-01-11 13:44:25 +0800

    Leading zeros

--- Calcu - Assignment 1/Form1.vb
+++ Calcu - Assignment 1/Form1.vb
@@ -29,38 +29,47 @@
     End Sub
 
     Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("8")
     End Sub
 
     Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("9")
     End Sub
 
     Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("4")
     End Sub
 
     Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("5")
     End Sub
 
     Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("6")
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("1")
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("2")
     End Sub
 
     Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("3")
     End Sub
 
     Private Sub Button33_Click(sender As Object, e As EventArgs) Handles Button33.Click
+        textInput.Text = textInput.Text.TrimStart("0"c)
         textInput.Text += ("0")
     End Sub
 
